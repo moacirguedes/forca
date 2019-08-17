c6432d785e9b3f1cf86c2fc33c675e673c9cd4e2
Moacir <guedesmoacir12@gmail.com>
2019-08-17 03:12:41 -0300

    added basic flow of the game

--- game.rb
+++ game.rb
@@ -3,38 +3,85 @@
 class Game
   attr_reader :ongoing
 
-  @hit_last = false
-  @ongoing = true
-
-  def initialize
+  def initialize(word)
     @lives = 3
     @attempts = 0
-    @points = 0
+    @word = word
+    @secret_word = '-' * word.length # how to use _ D:
+    @hit_last = false
+    @ongoing = true
+    @alphabet = %w[A B C D E F G H I J K
+                   L M N O P Q R S T U V W X Y Z]
   end
 
   def to_s
-    "You have #{@lives} lives left and you made #{@attempts} attempts."
+    <<~ENDTEXT
+      You got #{@lives} lives left and you made #{@attempts} attempts.
+
+      You still got those letters to try #{@alphabet}
+
+      The secret word is #{@secret_word}  (#{@word.length} letters)
+
+    ENDTEXT
+  end
+
+  def try(attempt)
+    score(attempt) unless check_alphabet(attempt)
+  end
+
+  def score(attempt)
+    if attempt.length == 1 && @word[attempt]
+      increase_lives
+      update_secret_word(attempt)
+    elsif attempt == @word
+      increase_attempts
+      win_game
+    else
+      decrease_lives
+      delete_alphabet(attempt) if attempt.length == 1
+    end
   end
 
   def increase_attempts
     @attempts += 1
   end
 
-  def increase_points
+  def check_alphabet(letter)
+    true unless @alphabet.include?(letter) || letter.length != 1
+  end
+
+  def delete_alphabet(letter)
+    @alphabet.delete(letter)
+  end
+
+  def increase_lives
     increase_attempts
-    @points += 1 if @hit_last && @points < 3
+    @lives += 1 if @hit_last && @lives < 3
     @hit_last = true
   end
 
-  def decrease_points
+  def decrease_lives
     increase_attempts
-    @points -= 1 if @points.positive?
+    @lives -= 1 if @lives.positive?
     @hit_last = false
-    end_game if @points.zero?
+    end_game if @lives.zero?
+  end
+
+  def update_secret_word(attempt)
+    delete_alphabet(attempt)
+    @secret_word.each_char.with_index do |_char, index|
+      @secret_word[index] = attempt.upcase if @word[index] == attempt
+    end
+    win_game unless @secret_word.include?('-')
+  end
+
+  def win_game
+    @ongoing = false
+    puts "You won with #{@attempts} attempts"
   end
 
   def end_game
     @ongoing = false
-    puts 'you lost :D'
+    puts "you lose with #{@attempts} attempts. The secret word was #{@word}"
   end
 end
--- hangman.rb
+++ hangman.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'game.rb'
+require './game.rb'
 
 words = []
 
@@ -8,4 +8,39 @@ File.open('categories.txt') do |file|
   file.each do |line|
     words << line.upcase.split(/\W+/) if line[0] != '#' && line[0] != "\n"
   end
+end
+
+system 'clear'
+
+puts <<~ENDTEXT
+  Welcome to Hangcir :D
+  You have 3 lifes, each turn you can try a whole word or just one letter
+  If you miss a try you lose one life, otherwise if you hit twice in a row you will earn one life (max 3)
+  If you lose all your lives, game over.
+
+  Please type one category number from the listed below to start the game
+
+ENDTEXT
+
+# TODO: check here if txt is wrong or empty
+
+words.each_with_index do |line, index|
+  puts "#{index} #{line[0]}"
+end
+
+user_number = gets.chomp.to_i
+
+while user_number > words.length - 1 || user_number.negative?
+  puts "Please insert a correct number (0 to #{words.length - 1})"
+  user_number = gets.chomp.to_i
+end
+
+# is this gambiarra or ugly? (and i was doing game = new Game() ._.)
+game = Game.new(words[user_number][rand(1..words[user_number].length - 1)])
+
+while game.ongoing
+  system 'clear'
+  puts game
+  puts 'Please insert one letter or a whole word to make a try'
+  game.try(gets.chomp.upcase)
 end
